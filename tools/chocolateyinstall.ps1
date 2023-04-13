d45200828e1e3c565bbd3b04e05644ff874f2184
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-04-13 10:10:57 -0400

    Update to 4.4.2304.1201

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
 $softwareName = 'XSplit Broadcaster'
-$softwareVersion = '4.4.2301.0904'
+$softwareVersion = '4.4.2304.1201'
 $shouldInstall = Get-ShouldInstall -Version $softwareVersion
 
 if (!$shouldInstall -and !$env:ChocolateyForce)
@@ -29,8 +29,8 @@ else
     packageName    = $env:ChocolateyPackageName
     softwareName   = $softwareName
     fileType       = 'EXE'
-    url64bit       = 'https://cdn2.xsplit.com/download/bc/m49/4.4.2301.0904/XSplit_Broadcaster_4.4.2301.0904.exe'
-    checksum64     = '13cae70095b253768841d9dd658384b67630daea5163335d570dca69c7a01e76'
+    url64bit       = 'https://cdn2.xsplit.com/download/bc/m50/4.4.2304.1201/XSplit_Broadcaster_4.4.2304.1201.exe'
+    checksum64     = 'ac34504a1e51f1429a5bcdbca23e3c919e17b159e0f5c5b5ef8231cf3d9fba3a'
     checksumType64 = 'sha256'
     silentArgs     = "/exenoui /noprereqs /exelog `"$logFilePathPrefix.ExeInstall.log`" /qn /norestart /l*v `"$logFilePathPrefix.MsiInstall.log`""
     validExitCodes = @(0, 3010, 1641)
