73aeb83c92e61235c84849df466622e27514da67
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-07-11 00:59:36 -0400

    Experimental support for internal builds

--- update.ps1
+++ update.ps1
@@ -1,5 +1,7 @@
 Import-Module au
 
+$userAgent = "Update checker of Chocolatey Community Package 'xsplit-broadcaster'"
+
 function global:au_BeforeUpdate ($Package) {
     #Archive this version for future development, since the vendor does not guarantee perpetual availability
     $filePath = ".\XSplit_Broadcaster_$($Latest.Version).exe"
@@ -36,35 +38,102 @@ function global:au_SearchReplace {
     }
 }
 
-function Get-LatestReleaseData {
-    $uri = 'https://www.xsplit.com/api/service/download?page_size=10&application_id=1&active=1&release=0&platform=windows&installer_type=exe'
-    $userAgent = "Update checker of Chocolatey Community Package 'xsplit-broadcaster'"
+function Get-LatestPublicReleaseData {
+    $canonicalUri = 'https://www.xsplit.com/api/service/download?page_size=10&application_id=1&active=1&release=0&platform=windows&installer_type=exe'
 
-    $response = Invoke-RestMethod -Uri $uri -UserAgent $userAgent -UseBasicParsing
+    $response = Invoke-RestMethod -Uri $canonicalUri -UserAgent $userAgent -UseBasicParsing
 
     return $response.data[0]
 }
 
-function global:au_GetLatest {
-    $releaseData = Get-LatestReleaseData
-    $version = $releaseData.version
-
+function Get-OfflineInstallerUri([uri] $WebInstallerUri) {
     #The package uses the offline installer, but SplitMediaLabs only publishes the web installer's URI.
     #This is only publicly shared via the web installer, and exposed as an alternate location in the installer.
-    #We may need to cross-check against it or probe the server every now and then to keep up with changes.
-    $webInstallerUri = ([System.Uri] $releaseData.download_url)
-    $webInstallerUriSegments = $webInstallerUri.Segments
-    $downloadUrlDirectory = $webInstallerUri.AbsoluteUri.TrimEnd($webInstallerUriSegments[$webInstallerUriSegments.Length - 1])
+    $webInstallerUriSegments = $WebInstallerUri.Segments
+    $downloadUrlDirectory = $WebInstallerUri.AbsoluteUri.TrimEnd($webInstallerUriSegments[$webInstallerUriSegments.Length - 1])
+    $version = $webInstallerUriSegments[4].TrimEnd('/')
+
+    #SplitMediaLabs sometimes puts the web installer in a different directory than the offline installer.
+    #Probe the server to confirm we have a valid URL before returning it.
+    $sameDirectoryUri = "$($downloadUrlDirectory)XSplit_Broadcaster_$version.exe"
+
+    $returnedUri = $null
+    try {
+        Invoke-WebRequest -Uri $sameDirectoryUri -Method Head -UserAgent $userAgent | Out-Null
+        $returnedUri = $sameDirectoryUri
+    }
+    catch {
+        if ($_.Exception.Response.StatusCode.Value__ -eq 404) {
+            #Assuming we have a web installer within a "/1/" directory. Remove this from the path.
+            $sameDirectoryUriSegments = ([uri] $sameDirectoryUri).Segments
+            $differentDirectory = $sameDirectoryUri.TrimEnd($sameDirectoryUriSegments[$sameDirectoryUriSegments.Length - 1]).TrimEnd($sameDirectoryUriSegments[$sameDirectoryUriSegments.Length - 2])
+            $differentDirectoryUri = "$($differentDirectory)/XSplit_Broadcaster_$version.exe"
+            Invoke-WebRequest -Uri $differentDirectoryUri -Method Head -UserAgent $userAgent | Out-Null
+            $returnedUri = $differentDirectoryUri
+        }
+    }
+
+    return $returnedUri
+}
+
+function Get-LatestInternalReleaseInfo($M) {
+    $canonicalUri = "https://xspl.it/bc/$M/latest"
+    $response = Invoke-WebRequest -Uri $canonicalUri -UserAgent $userAgent -MaximumRedirection 0 -SkipHttpErrorCheck -UseBasicParsing -ErrorAction SilentlyContinue
+    
+    $redirectedUri = $response.Headers['Location'][0]
+    if ($redirectedUri -eq 'https://www.xsplit.com/') {
+        throw "$M is not a valid release ID!"
+    }
+    $version = Get-Version -Version $redirectedUri
+
+    return @{
+        ReleaseNotes    = '' #Internal releases are not publicly documented
+        SoftwareVersion = $version
+        URL64           = Get-OfflineInstallerUri -WebInstallerUri $redirectedUri
+        Version         = "$version-internal" #This may change if building a package fix version
+    }
+}
+
+function Get-LatestPublicReleaseInfo {
+    $releaseData = Get-LatestPublicReleaseData
+    $version = $releaseData.version
 
     return @{
         ReleaseNotes    = $releaseData.release_notes_url
         SoftwareVersion = $version
-        URL64           = "$($downloadUrlDirectory)XSplit_Broadcaster_$version.exe"
+        URL64           = Get-OfflineInstallerUri -WebInstallerUri $releaseData.download_url
         Version         = $version #This may change if building a package fix version
     }
 }
 
-$releaseData = Get-LatestReleaseData
+function global:au_GetLatest {
+    $streams = [ordered] @{
+        Stable = Get-LatestPublicReleaseInfo
+    }
+
+    $m = 43;
+    while ($true) {
+        $keyName = "m$m"
+        try {
+            $streams.Add($keyName, (Get-LatestInternalReleaseInfo -M $keyName))
+            $m++
+        }
+        catch {
+            if ($_.Exception.Message -eq "$keyName is not a valid release ID!") {
+                #We're done enumerating releases.
+                break;
+            }
+            else {
+                #Rethrow original exception
+                throw
+            }
+        }
+    }
+
+    return @{ Streams = $streams }
+}
+
+$releaseData = Get-LatestPublicReleaseData
 $latestPublishedVersion = $releaseData.Version
 
 $currentPath = (Split-Path $MyInvocation.MyCommand.Definition)
