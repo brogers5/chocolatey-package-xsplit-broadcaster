9eff73bc7de6c93ff344b27394c1a43e7ada0c15
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-12-11 08:55:57 -0500

    Extract uninstall code to helper function

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,27 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
 
-$packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  softwareName   = 'XSplit Broadcaster'
-  fileType       = 'MSI'
-  silentArgs     = '/qn /norestart'
-  validExitCodes = @(0, 3010, 1605, 1614, 1641)
-}
-
-[array] $key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
-
-if ($key.Count -eq 1) {
-  $packageArgs['silentArgs'] = "$($key[0].PSChildName) $($packageArgs['silentArgs'])"
-  $packageArgs['file'] = ''
-
-  Uninstall-ChocolateyPackage @packageArgs
-}
-elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-}
-elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning 'To prevent accidental data loss, no programs will be uninstalled.'
-  Write-Warning 'Please alert package maintainer the following keys were matched:'
-  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
-}
+Uninstall-CurrentVersion
