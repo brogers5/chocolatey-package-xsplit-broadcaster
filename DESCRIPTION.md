0943bd0d5a3aeeb62a23c77526ac59a100f04445
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-09-04 14:11:13 -0400

    Update to 4.5.2508.1809

--- DESCRIPTION.md
+++ DESCRIPTION.md
@@ -6,4 +6,4 @@ XSplit Broadcaster, the best all-in-one streaming and recording application for 
 
 ---
 
-## [Package Notes](https://github.com/brogers5/chocolatey-package-xsplit-broadcaster/blob/v4.5.2508.1805/PACKAGE-NOTES.md)
+## [Package Notes](https://github.com/brogers5/chocolatey-package-xsplit-broadcaster/blob/v4.5.2508.1809/PACKAGE-NOTES.md)
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
 $softwareName = 'XSplit Broadcaster'
-[version] $softwareVersion = '4.5.2508.1805'
+[version] $softwareVersion = '4.5.2508.1809'
 $currentVersion = Get-CurrentVersion
 
 if ($currentVersion -eq $softwareVersion -and !$env:ChocolateyForce) {
@@ -31,8 +31,8 @@ else {
     packageName    = $env:ChocolateyPackageName
     softwareName   = $softwareName
     fileType       = 'EXE'
-    url64bit       = 'https://cdn.xsplit.com/download/bc/m54/4.5.2508.1805/XSplit_Broadcaster_4.5.2508.1805.exe'
-    checksum64     = '43e52c53c847f5ffdcbe68af9433c9e31ad137e6ea61ba4bdcddf4f7b31c1266'
+    url64bit       = 'https://cdn.xsplit.com/download/bc/m54/4.5.2508.1809/XSplit_Broadcaster_4.5.2508.1809.exe'
+    checksum64     = '96705a60b32ea251cb829cbfac88a91fa865fae8c87bc7b45997979d32585d38'
     checksumType64 = 'sha256'
     silentArgs     = "/exenoui /noprereqs /exelog `"$logFilePathPrefix.ExeInstall.log`" /qn /norestart /l*v `"$logFilePathPrefix.MsiInstall.log`""
     validExitCodes = @(0, 3010, 1641)
